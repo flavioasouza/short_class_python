014082f432dfb50156eeb1c904679d88c211604c
flavioasouza <84750489+flavioasouza@users.noreply.github.com>
2021-12-15 21:44:30 -0300

    Update valor_monetario.py

--- valor_monetario.py
+++ valor_monetario.py
@@ -2,7 +2,7 @@
 def deprecia_valor():
     valor_atual = float(input("Digite um valor monetário: "))
     print(f"O valor atual é: {valor_atual}")
-    valor_depreciado = valor_atual * 0.75
+    valor_depreciado = valor_atual * 0.85
     print(f"O valor após depreciação de 15% é: {valor_depreciado}")
 
 print(deprecia_valor())
